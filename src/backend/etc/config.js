23fc2de65be37be421ecd270302fd2078c90c700
evan <281406674@qq.com>
2018-06-19 14:57:01 +0800

    linux配置文件修改
    Signed-off-by: evan <281406674@qq.com>

--- src/backend/etc/config.js
+++ src/backend/etc/config.js
@@ -8,7 +8,6 @@ exports.prop = {
 	"server_path":'E:/evan/nodejs/bcos-server',//***项目路径，windows linux有分别
 	"file_split":"/",//***windows linux有分别
 	"log_path":'/src/backend/log',//***log4js日志路径，windows linux有分别
-    "download_path":'/src/backend/log',//***log4js日志路径，windows linux有分别
 	"log_level":"info",//***log4js日志级别，正式环境和测试环境有分别
 	"codeline":true//日志打印时，是否显示代码行数开关
 };
